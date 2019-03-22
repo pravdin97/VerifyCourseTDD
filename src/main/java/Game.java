671f353848554c09e373fcb452b207ef2c3190aa
pravdin97 <pravdin.ev97@gmail.com>
2019-03-22 16:25:40 +0700

    Add getNextTask() trivial implementation. Test pass successfully

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,12 @@
 public class Game {
+    public Game() {
+    }
+
+    public Task getNextTask() {
+        return new Task();
+    }
+
+    public boolean hasNextTask() {
+        return true;
+    }
 }
